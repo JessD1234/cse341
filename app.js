073e56ad629a6a9e20f238399cc16ca45f074799
JessD1234 <46542907+JessD1234@users.noreply.github.com>
2021-09-23 21:38:52 -0600

    commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('views/404', {pageTitle: 'Page Not Found'});
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(PORT);
